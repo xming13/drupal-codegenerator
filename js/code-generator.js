18a11c5453a4bd4698986f7093fcc43c391c3118
xming13 <xiaoming33@gmail.com>
2015-03-06 08:48:37 +0800

    add hook_entity_info

--- js/code-generator.js
+++ js/code-generator.js
@@ -2,119 +2,179 @@ $(document).ready(function () {
 
     Backbone.Form.editors.List.Modal.ModalAdapter = Backbone.BootstrapModal;
 
-    var field = Backbone.Model.extend({
-        schema: {
-            name: 'Text',
-            description: 'Text',
-            type: {type: 'Select', options: ['serial', 'int', 'varchar']},
-            unsigned: {
-                type: 'Radio',
-                options: {
-                    value1: 'TRUE',
-                    value2: 'FALSE'
-                }
+    initFormHookSchema();
+    initFormHookEntityInfo();
+
+    function initFormHookSchema() {
+        var field = Backbone.Model.extend({
+            schema: {
+                name: 'Text',
+                description: 'Text',
+                type: {type: 'Select', options: ['serial', 'int', 'varchar']},
+                unsigned: {
+                    type: 'Radio',
+                    options: {
+                        value1: 'TRUE',
+                        value2: 'FALSE'
+                    }
+                },
+                'not null': {
+                    type: 'Radio',
+                    options: {
+                        value1: 'TRUE',
+                        value2: 'FALSE'
+                    }
+                },
+                default: 'Text'
             },
-            'not null': {
-                type: 'Radio',
-                options: {
-                    value1: 'TRUE',
-                    value2: 'FALSE'
-                }
+
+            toString: function() {
+                return JSON.stringify(this);
+            }
+        });
+
+        var index = Backbone.Model.extend({
+            schema: {
+                name: { type: 'Text', validators: ['required'] },
+                'Column Names': { type: 'List', itemType: 'Text', validators: ['required'] }
             },
-            default: 'Text'
-        },
+            toString: function() {
+                return JSON.stringify(this);
+            }
+        });
 
-        toString: function() {
-           return JSON.stringify(this);
-        }
-    });
+        var uniqueKey = Backbone.Model.extend({
+            schema: {
+                name: 'Text',
+                'Column Names': { type: 'List', itemType: 'Text' }
+            },
+            toString: function() {
+                return JSON.stringify(this);
+            }
+        });
 
-    var index = Backbone.Model.extend({
-        schema: {
-            name: { type: 'Text', validators: ['required'] },
-            'Column Names': { type: 'List', itemType: 'Text', validators: ['required'] }
-        },
-        toString: function() {
-            return JSON.stringify(this);
-        }
-    });
+        var foreignKeyColumnMap = Backbone.Model.extend({
+            schema: {
+                'Column name of referencing table': 'Text',
+                'Column name of referenced table': 'Text'
+            },
+            toString: function() {
+                return JSON.stringify(this);
+            }
+        });
+        var foreignKeyDetail = Backbone.Model.extend({
+            schema: {
+                table: 'Text',
+                columns: { type: 'List', itemType: 'NestedModel', model: foreignKeyColumnMap }
+            },
+            toString: function() {
+                return JSON.stringify(this);
+            }
+        });
 
-    var uniqueKey = Backbone.Model.extend({
-        schema: {
-            name: 'Text',
-            'Column Names': { type: 'List', itemType: 'Text' }
-        },
-        toString: function() {
-            return JSON.stringify(this);
-        }
-    });
+        var foreignKey = Backbone.Model.extend({
+            schema: {
+                name: 'Text',
+                'Detail': { type: 'List', itemType: 'NestedModel', model: foreignKeyDetail }
+            },
+            toString: function() {
+                return JSON.stringify(this);
+            }
+        });
 
-    var foreignKeyColumnMap = Backbone.Model.extend({
-        schema: {
-            'Column name of referencing table': 'Text',
-            'Column name of referenced table': 'Text'
-        },
-        toString: function() {
-            return JSON.stringify(this);
-        }
-    });
-    var foreignKeyDetail = Backbone.Model.extend({
-        schema: {
-            table: 'Text',
-            columns: { type: 'List', itemType: 'NestedModel', model: foreignKeyColumnMap }
-        },
-        toString: function() {
-            return JSON.stringify(this);
-        }
-    });
+        var dbSchema = Backbone.Model.extend({
+            schema: {
+                'Table Name': 'Text',
+                description: 'Text',
+                fields: { type: 'List', itemType: 'NestedModel', model: field },
+                indexes: { type: 'List', itemType: 'NestedModel', model: index },
+                'Unique Keys': { type: 'List', itemType: 'NestedModel', model: uniqueKey },
+                'Foreign Keys': { type: 'List', itemType: 'NestedModel', model: foreignKey },
+                'Primary Key': { type: 'List', itemType: 'Text' }
+            },
+            toString: function() {
+                return JSON.stringify(this);
+            }
+        });
 
-    var foreignKey = Backbone.Model.extend({
-        schema: {
-            name: 'Text',
-            'Detail': { type: 'List', itemType: 'NestedModel', model: foreignKeyDetail }
-        },
-        toString: function() {
-            return JSON.stringify(this);
-        }
-    });
+        var dbSchemas = Backbone.Model.extend({
+            schema: {
+                schemas: { type: 'List', itemType: 'NestedModel', model: dbSchema }
+            }
+        });
 
-    var dbSchema = Backbone.Model.extend({
-        schema: {
-            'Table Name': 'Text',
-            description: 'Text',
-            fields: { type: 'List', itemType: 'NestedModel', model: field },
-            indexes: { type: 'List', itemType: 'NestedModel', model: index },
-            'Unique Keys': { type: 'List', itemType: 'NestedModel', model: uniqueKey },
-            'Foreign Keys': { type: 'List', itemType: 'NestedModel', model: foreignKey },
-            'Primary Key': { type: 'List', itemType: 'Text' }
-        },
-        toString: function() {
-            return JSON.stringify(this);
-        }
-    });
+        var dbSchemaModel = new dbSchemas();
 
-    var dbSchemas = Backbone.Model.extend({
-        schema: {
-            schemas: { type: 'List', itemType: 'NestedModel', model: dbSchema }
-        }
-    });
+        var form = new Backbone.Form({
+            model: dbSchemaModel
+        }).render();
 
-    var dbSchemas = new dbSchemas();
+        form.on('change', function(form, editor) {
+            form.commit();
+            $('#code-hook-schema pre').html(
+                syntaxHighlight(
+                    JSON.stringify(dbSchemaModel.toJSON(), null, 4)
+                )
+            );
+        });
 
-    var form = new Backbone.Form({
-        model: dbSchemas
-    }).render();
+        $('#form-hook-schema').append(form.el);
+    }
 
-    form.on('change', function(form, editor) {
-        form.commit();
-        $('#hook-schema pre').html(
-            syntaxHighlight(
-                JSON.stringify(dbSchemas.toJSON(), null, 4)
-            )
-        );
-    });
+    function initFormHookEntityInfo() {
+        var entityInfo = Backbone.Model.extend({
+            schema: {
+                'label': 'Text',
+                'controller class': 'Text',
+                'base table': 'Text',
+                'revision table': 'Text',
+                'static cache': {
+                    type: 'Radio',
+                    options: ['TRUE', 'FALSE']
+                },
+                'field cache': {
+                    type: 'Radio',
+                    options: ['TRUE', 'FALSE']
+                },
+                'load hook': 'Text',
+                'uri callback': 'Text',
+                'label callback': 'Text',
+                'language callback': 'Text',
+                'fieldable': {
+                    type: 'Radio',
+                    options: ['TRUE', 'FALSE']
+                },
+                'translation': 'Text',
+                'entity keys': 'Text',
+                'bundle keys': 'Text',
+                'bundles': 'Text',
+                'view modes': 'Text'
+            },
+            defaults: {
+                'static cache': 'FALSE',
+                'field cache': 'TRUE'
+            },
+            toString: function() {
+                var jsonString = JSON.stringify(this.toJSON(), null, 4);
+                return jsonString.replace("{", "array(").replace('}', ')');
+            }
+        });
 
-    $('#form').append(form.el);
+        var entityInfoModel = new entityInfo();
+
+        var form = new Backbone.Form({
+            model: entityInfoModel
+        }).render();
+
+        form.on('change', function(form, editor) {
+            form.commit();
+            $('#code-hook-entity-info pre').html(
+                syntaxHighlight(entityInfoModel.toString())
+            );
+        });
+
+        $('#form-hook-entity-info').append(form.el);
+    }
 
     function syntaxHighlight(json) {
         if (typeof json != 'string') {
