70da66ac625e4dec6838b064f6f388349ba40bc5
xming13 <xiaoming33@gmail.com>
2015-03-07 14:06:05 +0800

    Implement auto generate code and add highlight

--- js/main.js
+++ js/main.js
@@ -3,19 +3,23 @@ requirejs.config({
         "jquery": "../bower_components/jquery/dist/jquery.min",
         "bootstrap": "../bower_components/bootstrap/dist/js/bootstrap.min",
         "underscore": "../bower_components/underscore/underscore",
-        "underscore.string": "../bower_components/underscore.string/dist/underscore.string.min",
+        "underscore.string": "../bower_components/underscore.string/dist/underscore.string",
         "backbone": "../bower_components/backbone/backbone",
         "localStorage": "../bower_components/backbone.localStorage/backbone.localStorage-min",
         "backbone-forms": "../bower_components/backbone-forms/distribution.amd/backbone-forms.min",
         "backbone-forms.list": "../bower_components/backbone-forms/distribution.amd/editors/list.min",
         "backbone-forms.bootstrap": "backbone-forms.bootstrap.custom",
         "backbone.bootstrap-modal": "backbone.bootstrap-modal.custom",
-        "json2": "../bower_components/json2/json2"
+        "json2": "../bower_components/json2/json2",
+        "highlight": "highlight.php"
     },
 
     shim: {
         underscore: {
             exports: "_"
+        },
+        'underscore.string': {
+            deps: ['underscore']
         },
         backbone: {
             deps: ["jquery", "underscore"/*, "json2"*/],
@@ -31,15 +35,19 @@ requirejs.config({
 
 require([
     'jquery',
+    'underscore',
     'backbone',
+    'underscore.string',
     'backbone-forms',
     'backbone-forms.list',
     "backbone-forms.bootstrap",
     'backbone.bootstrap-modal',
-    'bootstrap'
-], function($, Backbone) {
+    'bootstrap',
+    'highlight'
+], function ($, _, Backbone, s) {
 
     $(function () {
+
         Backbone.Form.editors.List.Modal.ModalAdapter = Backbone.BootstrapModal;
 
         initFormHookSchema();
@@ -68,7 +76,7 @@ require([
                     default: 'Text'
                 },
 
-                toString: function() {
+                toString: function () {
                     return JSON.stringify(this);
                 }
             });
@@ -78,7 +86,7 @@ require([
                     name: { type: 'Text', validators: ['required'] },
                     'Column Names': { type: 'List', itemType: 'Text', validators: ['required'] }
                 },
-                toString: function() {
+                toString: function () {
                     return JSON.stringify(this);
                 }
             });
@@ -88,7 +96,7 @@ require([
                     name: 'Text',
                     'Column Names': { type: 'List', itemType: 'Text' }
                 },
-                toString: function() {
+                toString: function () {
                     return JSON.stringify(this);
                 }
             });
@@ -98,7 +106,7 @@ require([
                     'Column name of referencing table': 'Text',
                     'Column name of referenced table': 'Text'
                 },
-                toString: function() {
+                toString: function () {
                     return JSON.stringify(this);
                 }
             });
@@ -107,7 +115,7 @@ require([
                     table: 'Text',
                     columns: { type: 'List', itemType: 'NestedModel', model: foreignKeyColumnMap }
                 },
-                toString: function() {
+                toString: function () {
                     return JSON.stringify(this);
                 }
             });
@@ -117,7 +125,7 @@ require([
                     name: 'Text',
                     'Detail': { type: 'List', itemType: 'NestedModel', model: foreignKeyDetail }
                 },
-                toString: function() {
+                toString: function () {
                     return JSON.stringify(this);
                 }
             });
@@ -132,7 +140,7 @@ require([
                     'Foreign Keys': { type: 'List', itemType: 'NestedModel', model: foreignKey },
                     'Primary Key': { type: 'List', itemType: 'Text' }
                 },
-                toString: function() {
+                toString: function () {
                     return JSON.stringify(this);
                 }
             });
@@ -149,7 +157,7 @@ require([
                 model: dbSchemaModel
             }).render();
 
-            form.on('change', function(form, editor) {
+            form.on('change', function (form, editor) {
                 form.commit();
                 $('#code-hook-schema pre').html(
                     syntaxHighlight(
@@ -164,30 +172,100 @@ require([
         function initFormHookEntityInfo() {
             var entityInfo = Backbone.Model.extend({
                 schema: {
-                    'machine_name': { type: 'Text', title: 'Machine Name', help: 'This will be used to auto generate some of the field values in this form.' },
-                    'label': { type: 'Text', title: 'Label', help: 'Provides a human-readable label.'},
-                    'controller_class': { type: 'Text', title: 'Controller Class' },
-                    'base_table': { type: 'Text', title: 'Base Table' },
-                    'revision_table': { type: 'Text', title: 'Revision Table' },
+                    'machine_name': {
+                        type: 'Text',
+                        title: 'Machine Name',
+                        help: 'This will be used to auto generate some of the field values in this form.',
+                        validators: [
+                            {
+                                type: 'regexp',
+                                regexp: /^[a-z_\x7f-\xff][a-z0-9_\x7f-\xff]*$/,
+                                message: 'The machine-readable name must contain only lowercase letters, numbers, and underscores. First character cannot be number.'
+                            }
+                        ]
+                    },
+                    'label': {
+                        type: 'Text',
+                        title: 'Label',
+                        help: 'The human-readable name of the type.'
+                    },
+                    'controller_class': {
+                        type: 'Text',
+                        title: 'Controller Class',
+                        help: 'The name of the class that is used to load the objects. The class has to implement the ' +
+                            '<a href="https://api.drupal.org/api/drupal/includes%21entity.inc/interface/DrupalEntityControllerInterface/7" target="_blank">DrupalEntityControllerInterface</a> ' +
+                            'interface. Leave blank to use the ' +
+                            '<a href="https://api.drupal.org/api/drupal/includes%21entity.inc/interface/DrupalEntityControllerInterface/7" target="_blank">DrupalDefaultEntityController</a> ' +
+                            'implementation.'
+                    },
+                    'base_table': {
+                        type: 'Text',
+                        title: 'Base Table',
+                        help: '(used by ' +
+                            '<a href="https://api.drupal.org/api/drupal/includes%21entity.inc/class/DrupalDefaultEntityController/7" target="_blank">DrupalDefaultEntityController</a>' +
+                            ') The name of the entity type\'s base table.'
+                    },
+                    'revision_table': {
+                        type: 'Text',
+                        title: 'Revision Table',
+                        help: 'The name of the entity type\'s revision table (if any).'
+                    },
                     'static_cache': {
                         type: 'Radio',
                         title: 'Static Cache',
-                        options: ['TRUE', 'FALSE']
+                        options: ['TRUE', 'FALSE'],
+                        help: '(used by ' +
+                            '<a href="https://api.drupal.org/api/drupal/includes%21entity.inc/class/DrupalDefaultEntityController/7" target="_blank">DrupalDefaultEntityController</a>' +
+                            ') FALSE to disable static caching of entities during a page request. Defaults to TRUE.'
                     },
                     'field_cache': {
                         type: 'Radio',
                         title: 'Field Cache',
-                        options: ['TRUE', 'FALSE']
+                        options: ['TRUE', 'FALSE'],
+                        help: '(used by Field API loading and saving of field data) FALSE to disable Field API\'s persistent cache of field data. Only recommended if a higher level persistent cache is available for the entity type. Defaults to TRUE.'
                     },
-                    'load_hook': { type: 'Text', title: 'Load Hook' },
-                    'uri_callback': { Type: 'Text', title: 'URI Callback' },
-                    'label_callback': { Type: 'Text', title: 'Label Callback' },
-                    'language_callback': { Type: 'Text', title: 'Language Callback' },
+                    'load_hook': {
+                        type: 'Text',
+                        title: 'Load Hook',
+                        help: 'The name of the hook which should be invoked by DrupalDefaultEntityController:attachLoad(), for example \'node_load\'.'
+                    },
+                    'uri_callback': {
+                        Type: 'Text',
+                        title: 'URI Callback',
+                        help: 'The name of an implementation of ' +
+                            '<a href="https://api.drupal.org/api/drupal/modules%21system%21system.api.php/function/callback_entity_info_uri/7" target="_blank">callback_entity_info_uri()</a>.'
+                    },
+                    'label_callback': {
+                        Type: 'Text',
+                        title: 'Label Callback',
+                        help: '(optional) The name of an implementation of ' +
+                            '<a href="https://api.drupal.org/api/drupal/modules%21system%21system.api.php/function/callback_entity_info_label/7" target="_blank">callback_entity_info_label()</a>' +
+                            ', which returns the label of the entity. The entity label is the main string associated with an entity; for example, the title of a node or the subject of a comment. ' +
+                            'If there is an entity object property that defines the label, then using the \'label\' element of the \'entity keys\' return value component suffices to provide this information (see below). ' +
+                            'Alternatively, specifying this callback allows more complex logic to determine the label of an entity. See also the ' +
+                            '<a href="https://api.drupal.org/api/drupal/includes%21common.inc/function/entity_label/7" target="_blank">entity_label()</a>' +
+                            ' function, which implements this logic.'
+                    },
+                    'language_callback': {
+                        Type: 'Text',
+                        title: 'Language Callback',
+                        help: '(optional) The name of an implementation of ' +
+                            '<a href="https://api.drupal.org/api/drupal/modules%21system%21system.api.php/function/callback_entity_info_language/7" target="_blank">callback_entity_info_language()</a>. ' +
+                            'In most situations, when needing to determine this value, inspecting a property named after the \'language\' element of the \'entity keys\' should be enough. ' +
+                            'The language callback is meant to be used primarily for temporary alterations of the property value: entity-defining modules are encouraged to always define a language property, ' +
+                            'instead of using the callback as main entity language source. In fact not having a language property defined is likely to prevent an entity from being queried by language. Moreover, given that ' +
+                            '<a href="https://api.drupal.org/api/drupal/includes%21common.inc/function/entity_language/7" target="_blank">entity_language()</a> ' +
+                            'is not necessarily used everywhere it would be appropriate, modules implementing the language callback should be aware that this might not be always called.'
+                    },
                     'fieldable': {
                         type: 'Radio',
-                        options: ['TRUE', 'FALSE']
+                        options: ['TRUE', 'FALSE'],
+                        help: 'Set to TRUE if you want your entity type to accept fields being attached to it.'
                     },
-                    'translation': 'Text',
+                    'translation': {
+                        type: 'Text',
+                        help: 'An associative array of modules registered as field translation handlers. Array keys are the module names, array values can be any data structure the module uses to provide field translation. Any empty value disallows the module to appear as a translation handler.'
+                    },
                     'entity_keys': { type: 'Text', title: 'Entity Keys' },
                     'bundle_keys': { type: 'Text', title: 'Bundle Keys' },
                     'bundles': 'Text',
@@ -195,11 +273,40 @@ require([
                 },
                 defaults: {
                     'static_cache': 'FALSE',
-                    'field_cache': 'TRUE'
+                    'field_cache': 'TRUE',
+                    'fieldable': 'TRUE'
                 },
-                toString: function() {
-                    var jsonString = JSON.stringify(this.toJSON(), null, 4);
-                    return jsonString.replace("{", "array(").replace('}', ')');
+                codeTemplate: _.template("function hook_entity_info() {\n\
+  $return = array(\n\
+    '<%= machine_name %>' => array(\n\
+      'label' => t('<%= label %>'),\n\
+<% if (controller_class) { %>\
+      'controller class' => '<%= controller_class %>',\n\
+<% } %>\
+      'base table' => '<%= base_table %>',\n\
+      'revision table' => '<%= revision_table %>',\n\
+      'static cache' => <%= static_cache %>,\n\
+      'field cache' => <%= field_cache %>,\n\
+      'load hook' => '<%= load_hook %>',\n\
+      'uri callback' => '<%= uri_callback %>',\n\
+<% if (label_callback) { %>\
+      'label callback' => '<%= label_callback %>',\n\
+<% } %>\
+<% if (language_callback) { %>\
+      'language callback' => '<%= language_callback %>',\n\
+<% } %>\
+      'fieldable' => <%= fieldable %>\n\
+<% if (translation) { %>\
+      'translation' => '<%= translation %>',\n\
+<% } %>\
+    ),\n\
+  );\n\
+  \n\
+  return $return;\n\
+}\
+"),
+                toCode: function () {
+                    return this.codeTemplate(this.attributes);
                 }
             });
 
@@ -209,36 +316,28 @@ require([
                 model: entityInfoModel
             }).render();
 
-            form.on('change', function(form, editor) {
+            form.on('machine_name:change', function (form, machineNameEditor) {
+                var machineName = machineNameEditor.getValue();
+                form.setValue({
+                    label: s.humanize(machineName),
+                    controller_class: s.classify(machineName) + 'Controller',
+                    base_table: machineName,
+                    revision_table: machineName + '_revision',
+                    load_hook: machineName + '_load'
+                });
+            });
+
+            form.on('change', function (form) {
+
                 form.commit();
-                $('#code-hook-entity-info pre').html(
-                    syntaxHighlight(entityInfoModel.toString())
-                );
+
+                $('#code-hook-entity-info pre code').html(entityInfoModel.toCode())
+                    .each(function (i, block) {
+                        hljs.highlightBlock(block);
+                    });
             });
 
             $('#form-hook-entity-info').append(form.el);
-        }
-
-        function syntaxHighlight(json) {
-            if (typeof json != 'string') {
-                json = JSON.stringify(json, undefined, 2);
-            }
-            json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-            return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-                var cls = 'number';
-                if (/^"/.test(match)) {
-                    if (/:$/.test(match)) {
-                        cls = 'key';
-                    } else {
-                        cls = 'string';
-                    }
-                } else if (/true|false/.test(match)) {
-                    cls = 'boolean';
-                } else if (/null/.test(match)) {
-                    cls = 'null';
-                }
-                return '<span class="' + cls + '">' + match + '</span>';
-            });
         }
     });
 });
